211aed1e73afd8eddfcffa4ef63a4b0444ab57f0
rick.rui <1192577559@qq.com>
2019-05-31 10:54:07 +0800

    提交

--- Views/home.tpl.php
+++ Views/home.tpl.php
@@ -30,29 +30,29 @@
     <div class="left_open">
         <a><i title="展开左侧栏" class="iconfont">&#xe699;</i></a>
     </div>
-    <ul class="layui-nav left fast-add" lay-filter="">
-        <li class="layui-nav-item">
-            <a href="javascript:;">+新增</a>
-            <dl class="layui-nav-child">
-                <!-- 二级菜单 -->
-                <dd>
-                    <a onclick="xadmin.open('最大化','http://www.baidu.com','','',true)">
-                        <i class="iconfont">&#xe6a2;</i>弹出最大化</a></dd>
-                <dd>
-                    <a onclick="xadmin.open('弹出自动宽高','http://www.baidu.com')">
-                        <i class="iconfont">&#xe6a8;</i>弹出自动宽高</a></dd>
-                <dd>
-                    <a onclick="xadmin.open('弹出指定宽高','http://www.baidu.com',500,300)">
-                        <i class="iconfont">&#xe6a8;</i>弹出指定宽高</a></dd>
-                <dd>
-                    <a onclick="xadmin.add_tab('在tab打开','member-list.html')">
-                        <i class="iconfont">&#xe6b8;</i>在tab打开</a></dd>
-                <dd>
-                    <a onclick="xadmin.add_tab('在tab打开刷新','member-del.html',true)">
-                        <i class="iconfont">&#xe6b8;</i>在tab打开刷新</a></dd>
-            </dl>
-        </li>
-    </ul>
+<!--    <ul class="layui-nav left fast-add" lay-filter="">-->
+<!--        <li class="layui-nav-item">-->
+<!--            <a href="javascript:;">+新增</a>-->
+<!--            <dl class="layui-nav-child">-->
+<!--                <!-- 二级菜单 -->-->
+<!--                <dd>-->
+<!--                    <a onclick="xadmin.open('最大化','http://www.baidu.com','','',true)">-->
+<!--                        <i class="iconfont">&#xe6a2;</i>弹出最大化</a></dd>-->
+<!--                <dd>-->
+<!--                    <a onclick="xadmin.open('弹出自动宽高','http://www.baidu.com')">-->
+<!--                        <i class="iconfont">&#xe6a8;</i>弹出自动宽高</a></dd>-->
+<!--                <dd>-->
+<!--                    <a onclick="xadmin.open('弹出指定宽高','http://www.baidu.com',500,300)">-->
+<!--                        <i class="iconfont">&#xe6a8;</i>弹出指定宽高</a></dd>-->
+<!--                <dd>-->
+<!--                    <a onclick="xadmin.add_tab('在tab打开','member-list.html')">-->
+<!--                        <i class="iconfont">&#xe6b8;</i>在tab打开</a></dd>-->
+<!--                <dd>-->
+<!--                    <a onclick="xadmin.add_tab('在tab打开刷新','member-del.html',true)">-->
+<!--                        <i class="iconfont">&#xe6b8;</i>在tab打开刷新</a></dd>-->
+<!--            </dl>-->
+<!--        </li>-->
+<!--    </ul>-->
     <ul class="layui-nav right" lay-filter="">
         <li class="layui-nav-item">
             <a href="javascript:;">admin</a>
@@ -66,8 +66,8 @@
                     <a href="/logout">退出</a></dd>
             </dl>
         </li>
-        <li class="layui-nav-item to-index">
-            <a href="/">前台首页</a></li>
+<!--        <li class="layui-nav-item to-index">-->
+<!--            <a href="/">前台首页</a></li>-->
     </ul>
 </div>
 <!-- 顶部结束 -->
